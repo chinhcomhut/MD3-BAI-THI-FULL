e4f3ea116a9b61e28607301a817d60997c219436
- <chinhcomhut@gmail.com>
2019-11-18 18:15:08 +0700

    Bai Thi Full HD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ViewbookComponent} from './viewbook/viewbook.component';
 
 
-const routes: Routes = [{
-  path: 'content/:id',
-  component: ViewbookComponent
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { BookAddComponent } from './components/books/book-add/book-add.component
 import { BookEditComponent } from './components/books/book-edit/book-edit.component';
 import { BooksService} from './services/books.service';
 import {FormsModule} from '@angular/forms';
-import { ViewbookComponent } from './viewbook/viewbook.component';
+
 
 const appRoutes: Routes = [
   {
@@ -45,8 +45,6 @@ const appRoutes: Routes = [
     BookListComponent,
     BookAddComponent,
     BookEditComponent,
-    ViewbookComponent,
-
   ],
   imports: [
     BrowserModule,
